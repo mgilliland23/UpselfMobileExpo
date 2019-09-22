1ae0ce2e5a672e6200bd594c6426a04f4d6dd8ab
Matt Gilliland <mgilliland23@gmail.com>
2019-09-22 11:25:42 -0400

    created input scren for calm cloud

---
+++ screens/StressInputPage/index.js
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+//import "./Bounce_upsy.css";
+import { StyleSheet, View, Text, Dimensions } from "react-native";
+import StressTextInput from "../../components/StressTextInput";
+import { Button } from "react-native-paper";
+
+const win = Dimensions.get("window");
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    width: win.width,
+    //height: win.height,
+    padding: 1,
+    //alignSelf: 'flex-start',
+    backgroundColor: "#f2f2f2"
+  },
+  padding: {
+    flex: 1,
+    paddingHorizontal: 24,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  textInput: {
+    flex: 1
+  },
+  button: {
+    marginTop: 50,
+    marginBottom: 50
+  },
+  text: {
+    marginTop: 50,
+    marginBottom: 50,
+    fontSize: 20,
+    textAlign: "center"
+  }
+});
+
+export default class StressInputPage extends Component {
+  static navigationOptions = {
+    headerStyle: {
+      backgroundColor: "#6bccf3"
+    },
+    headerTintColor: "#fff",
+    headerTitleStyle: {
+      fontWeight: "bold"
+    }
+  };
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View style={styles.background}>
+        <View style={styles.padding}>
+          <Text style={styles.text}>
+            Tap the screen to type in something that is causing you stress
+          </Text>
+          <StressTextInput />
+          <Button
+            style={styles.button}
+            onPress={() => this.props.navigation.navigate("CalmCloud")}
+          >
+            Go!
+          </Button>
+        </View>
+      </View>
+    );
+  }
+}
