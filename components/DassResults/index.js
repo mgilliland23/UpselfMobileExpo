d7027299d8e9c803d585ed49157cac4c3f32ae36
Matt Gilliland <mgilliland23@gmail.com>
2019-09-26 19:50:07 -0400

    merge

--- components/DassResults/index.js
+++ components/DassResults/index.js
@@ -1,85 +1,85 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import {
   View,
   Text,
   ImageBackground,
   TouchableOpacity,
   StyleSheet,
-  Dimensions,
-} from 'react-native';
+  Dimensions
+} from "react-native";
 
-const win = Dimensions.get('window');
+const win = Dimensions.get("window");
 const styles = StyleSheet.create({
   background: {
     flex: 1,
-    width: win.width,
+    width: win.width
     // padding: 1,
   },
   upsyImg: {
     width: 100,
     height: 100,
-    alignSelf: 'center',
-    justifyContent: 'center',
-  },
+    alignSelf: "center",
+    justifyContent: "center"
+  }
 });
 
 export default class DassResults extends Component {
   // Depression Count
   calculateDepressionTotal = value => {
     if (value <= 9) {
-      return 'Normal';
+      return "Normal";
     } else if (value >= 10 && value <= 13) {
-      return 'Mild';
+      return "Mild";
     } else if (value >= 14 && value <= 20) {
-      return 'Moderate';
+      return "Moderate";
     } else if (value >= 21 && value <= 27) {
-      return 'Severe';
+      return "Severe";
     } else {
-      return 'Extremely Severe';
+      return "Extremely Severe";
     }
   };
 
   // Anxiety Count
   calculateAnxietyTotal = value => {
     if (value <= 7) {
-      return 'Normal';
+      return "Normal";
     } else if (value >= 8 && value <= 9) {
-      return 'Mild';
+      return "Mild";
     } else if (value >= 10 && value <= 14) {
-      return 'Moderate';
+      return "Moderate";
     } else if (value >= 15 && value <= 19) {
-      return 'Severe';
+      return "Severe";
     } else {
-      return 'Extremely Severe';
+      return "Extremely Severe";
     }
   };
 
   // Stress Count
   calculateStressTotal = value => {
     if (value <= 14) {
-      return 'Normal';
+      return "Normal";
     } else if (value >= 15 && value <= 18) {
-      return 'Mild';
+      return "Mild";
     } else if (value >= 19 && value <= 25) {
-      return 'Moderate';
+      return "Moderate";
     } else if (value >= 26 && value <= 33) {
-      return 'Severe';
+      return "Severe";
     } else {
-      return 'Extremely Severe';
+      return "Extremely Severe";
     }
   };
 
   allNormal = (dValue, aValue, sValue) => {
-      if (dValue<=9 && aValue<=7 && sValue<=14) {
-          return 'Normal'  
-      }
+    if (dValue <= 9 && aValue <= 7 && sValue <= 14) {
+      return "Normal";
+    }
   };
 
   render() {
-    const {navigation} = this.props;
-    const depressionCount = navigation.getParam('depressionCount', 0);
-    const anxietyCount = navigation.getParam('anxietyCount', 0);
-    const stressCount = navigation.getParam('stressCount', 0);
+    const { navigation } = this.props;
+    const depressionCount = navigation.getParam("depressionCount", 0);
+    const anxietyCount = navigation.getParam("anxietyCount", 0);
+    const stressCount = navigation.getParam("stressCount", 0);
 
     console.info(depressionCount);
     console.info(anxietyCount);
@@ -90,210 +90,235 @@ export default class DassResults extends Component {
         <View
           style={{
             flex: 1,
-            flexDirection: 'column',
-          }}>
-          <View style={{flex: 1}}>
+            flexDirection: "column"
+          }}
+        >
+          <View style={{ flex: 1 }}>
             <Text
               style={{
+                flex: 1,
                 marginTop: 40,
-                textAlign: 'center',
-                fontSize: 40,
-              }}>
-              DAS RESULTS
+                textAlign: "center",
+                fontSize: 40
+              }}
+            >
+              DASS-21 RESULTS
             </Text>
           </View>
           <View
             style={{
               flex: 2,
-            //   backgroundColor: '#6DCEF470',
-              marginTop: 30,
-            }}>
-            <View style={{marginTop: 10}}>
+              //   backgroundColor: '#6DCEF470',
+              marginTop: 30
+            }}
+          >
+            <View style={{ marginTop: 10 }}>
               <Text
                 style={{
-                  textAlign: 'center',
+                  textAlign: "center",
                   fontSize: 25,
-                  fontWeight: 'bold',
-                }}>
-                Depression Scale:{' '}
+                  fontWeight: "bold"
+                }}
+              >
+                Depression Scale:{" "}
                 {this.calculateDepressionTotal(depressionCount)}
               </Text>
             </View>
-            {this.calculateDepressionTotal(depressionCount) === 'Severe' && (
+            {this.calculateDepressionTotal(depressionCount) === "Severe" && (
               <TouchableOpacity
-                onPress={() => this.props.navigation.navigate('Chat')}>
+                onPress={() => this.props.navigation.navigate("Chat")}
+              >
                 <Text
                   style={{
-                    textAlign: 'center',
+                    textAlign: "center",
                     fontSize: 17,
-                    fontStyle: 'italic',
-                  }}>
+                    fontStyle: "italic"
+                  }}
+                >
                   Talk about it with Upsy
                 </Text>
                 <ImageBackground
                   style={styles.upsyImg}
-                  source={require('../../assets/images/menu_icons/chat.png')}
-                  resizeMode={'contain'}
+                  source={require("../../assets/images/menu_icons/chat.png")}
+                  resizeMode={"contain"}
                 />
               </TouchableOpacity>
             )}
             {this.calculateDepressionTotal(depressionCount) ===
-              'Extremely Severe' && (
+              "Extremely Severe" && (
               <TouchableOpacity
-                onPress={() => this.props.navigation.navigate('Chat')}>
+                onPress={() => this.props.navigation.navigate("Chat")}
+              >
                 <Text
                   style={{
-                    textAlign: 'center',
+                    textAlign: "center",
                     fontSize: 17,
-                    fontStyle: 'italic',
-                  }}>
+                    fontStyle: "italic"
+                  }}
+                >
                   Talk about it with Upsy
                 </Text>
                 <ImageBackground
                   style={styles.upsyImg}
-                  source={require('../../assets/images/menu_icons/chat.png')}
-                  resizeMode={'contain'}
+                  source={require("../../assets/images/menu_icons/chat.png")}
+                  resizeMode={"contain"}
                 />
               </TouchableOpacity>
             )}
           </View>
           <View
             style={{
-              flex: 2,
-            //   backgroundColor: '#6DCEF440',
-            }}>
-            <View style={{marginTop: 10}}>
+              flex: 2
+              //   backgroundColor: '#6DCEF440',
+            }}
+          >
+            <View style={{ marginTop: 10 }}>
               <Text
                 style={{
-                  textAlign: 'center',
+                  textAlign: "center",
                   fontSize: 25,
-                  fontWeight: 'bold',
-                }}>
+                  fontWeight: "bold"
+                }}
+              >
                 Anxiety Scale: {this.calculateAnxietyTotal(anxietyCount)}
               </Text>
             </View>
-            {this.calculateAnxietyTotal(anxietyCount) === 'Severe' && (
+            {this.calculateAnxietyTotal(anxietyCount) === "Severe" && (
               <TouchableOpacity
-                onPress={() => this.props.navigation.navigate('CalmCloud')}>
+                onPress={() => this.props.navigation.navigate("CalmCloud")}
+              >
                 <Text
                   style={{
-                    textAlign: 'center',
+                    textAlign: "center",
                     marginTop: 5,
                     fontSize: 17,
-                    fontStyle: 'italic',
-                  }}>
+                    fontStyle: "italic"
+                  }}
+                >
                   Check out our CalmCloud Exercise
                 </Text>
                 <ImageBackground
                   style={styles.upsyImg}
-                  source={require('../../assets/images/menu_icons/calmcloud.png')}
-                  resizeMode={'contain'}
+                  source={require("../../assets/images/menu_icons/calmcloud.png")}
+                  resizeMode={"contain"}
                 />
               </TouchableOpacity>
             )}
             {this.calculateAnxietyTotal(anxietyCount) ===
-              'Extremely Severe' && (
+              "Extremely Severe" && (
               <TouchableOpacity
-                onPress={() => this.props.navigation.navigate('CalmCloud')}>
+                onPress={() => this.props.navigation.navigate("CalmCloud")}
+              >
                 <Text
                   style={{
-                    textAlign: 'center',
+                    textAlign: "center",
                     marginTop: 5,
                     fontSize: 17,
-                    fontStyle: 'italic',
-                  }}>
+                    fontStyle: "italic"
+                  }}
+                >
                   Check out our CalmCloud Exercise
                 </Text>
                 <ImageBackground
                   style={styles.upsyImg}
-                  source={require('../../assets/images/menu_icons/calmcloud.png')}
-                  resizeMode={'contain'}
+                  source={require("../../assets/images/menu_icons/calmcloud.png")}
+                  resizeMode={"contain"}
                 />
               </TouchableOpacity>
             )}
           </View>
           <View
             style={{
-              flex: 2,
-            //   backgroundColor: '#6DCEF430',
-            }}>
-            <View style={{marginTop: 10}}>
+              flex: 2
+              //   backgroundColor: '#6DCEF430',
+            }}
+          >
+            <View style={{ marginTop: 10 }}>
               <Text
                 style={{
-                  textAlign: 'center',
+                  textAlign: "center",
                   fontSize: 25,
-                  fontWeight: 'bold',
-                }}>
+                  fontWeight: "bold"
+                }}
+              >
                 Stress Scale: {this.calculateStressTotal(stressCount)}
               </Text>
             </View>
-            {this.calculateStressTotal(stressCount) === 'Severe' && (
+            {this.calculateStressTotal(stressCount) === "Severe" && (
               <TouchableOpacity
-                onPress={() => this.props.navigation.navigate('Memory')}>
+                onPress={() => this.props.navigation.navigate("Memory")}
+              >
                 <Text
                   style={{
-                    textAlign: 'center',
+                    textAlign: "center",
                     marginTop: 5,
                     fontSize: 17,
-                    fontStyle: 'italic',
-                  }}>
+                    fontStyle: "italic"
+                  }}
+                >
                   Enter the Arcade Room!
                 </Text>
                 <ImageBackground
                   style={styles.upsyImg}
-                  source={require('../../assets/images/menu_icons/arcade.png')}
-                  resizeMode={'contain'}
+                  source={require("../../assets/images/menu_icons/arcade.png")}
+                  resizeMode={"contain"}
                 />
               </TouchableOpacity>
             )}
-            {this.calculateStressTotal(stressCount) === 'Extremely Severe' && (
+            {this.calculateStressTotal(stressCount) === "Extremely Severe" && (
               <TouchableOpacity
-                onPress={() => this.props.navigation.navigate('Memory')}>
+                onPress={() => this.props.navigation.navigate("Memory")}
+              >
                 <Text
                   style={{
-                    textAlign: 'center',
+                    textAlign: "center",
                     marginTop: 5,
                     fontSize: 17,
-                    fontStyle: 'italic',
-                  }}>
+                    fontStyle: "italic"
+                  }}
+                >
                   Enter the Arcade Room!
                 </Text>
                 <ImageBackground
                   style={styles.upsyImg}
-                  source={require('../../assets/images/menu_icons/arcade.png')}
-                  resizeMode={'contain'}
+                  source={require("../../assets/images/menu_icons/arcade.png")}
+                  resizeMode={"contain"}
                 />
               </TouchableOpacity>
             )}
           </View>
-          <View style={{flex: 1, justifyContent: 'center'}}>
-          {this.allNormal(depressionCount, anxietyCount, stressCount) === 'Normal' && (
-                <Text
-                  style={{
-                    textAlign: 'center',
-                    fontSize: 20,
-                    fontStyle: 'italic',
-                    marginBottom: 10,
-                  }}>
-                  You seem to be doing just fine, yay!
-                </Text>
+          <View style={{ flex: 1, justifyContent: "center" }}>
+            {this.allNormal(depressionCount, anxietyCount, stressCount) ===
+              "Normal" && (
+              <Text
+                style={{
+                  textAlign: "center",
+                  fontSize: 20,
+                  fontStyle: "italic",
+                  marginBottom: 10
+                }}
+              >
+                You seem to be doing just fine, yay!
+              </Text>
             )}
             <View>
-                <TouchableOpacity
+              <TouchableOpacity
                 style={{
-                    marginLeft: 150,
-                    marginRight: 150,
+                  marginLeft: 150,
+                  marginRight: 150
                 }}
-                onPress={() => this.props.navigation.navigate('Menu')}>
+                onPress={() => this.props.navigation.navigate("Menu")}
+              >
                 <Text
-                    style={{
-                    fontWeight: 'bold',
+                  style={{
+                    fontWeight: "bold",
                     fontSize: 20,
-                    textAlign: 'center',
-                    }}>
-                    OK
+                    textAlign: "center"
+                  }}
+                >
+                  OK
                 </Text>
-                </TouchableOpacity>
+              </TouchableOpacity>
             </View>
           </View>
         </View>
--- screens/StressTest/index.js
+++ screens/StressTest/index.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import {
   StyleSheet,
   View,
@@ -9,51 +9,51 @@ import {
   Dimensions,
   TouchableOpacity,
   Animated,
-  Modal,
-} from 'react-native';
-import dassQuestions from './questions';
-import DassResponse from '../../components/DassResponse';
-import DassResults from '../../components/DassResults';
+  Modal
+} from "react-native";
+import dassQuestions from "./questions";
+import DassResponse from "../../components/DassResponse";
+import DassResults from "../../components/DassResults";
 
-const win = Dimensions.get('window');
+const win = Dimensions.get("window");
 const styles = StyleSheet.create({
   background: {
     flex: 1,
-    width: win.width,
+    width: win.width
     // padding: 1,
   },
   logo: {
     flex: 1,
-    width: '97%',
-    height: '100%',
-    marginLeft: '3%',
-    justifyContent: 'center',
-    alignSelf: 'center',
+    width: "97%",
+    height: "100%",
+    marginLeft: "3%",
+    justifyContent: "center",
+    alignSelf: "center"
   },
   questionContainer: {
     flex: 1,
-    flexDirection: 'column',
-    display: 'flex',
+    flexDirection: "column",
+    display: "flex",
     height: 250,
     width: 250,
     marginLeft: 35,
-    alignSelf: 'center',
-    justifyContent: 'center',
-    textAlign: 'center',
-    alignItems: 'center',
+    alignSelf: "center",
+    justifyContent: "center",
+    textAlign: "center",
+    alignItems: "center"
   },
   upsyImg: {
     width: 100,
     height: 100,
-    alignSelf: 'center',
-    justifyContent: 'center',
+    alignSelf: "center",
+    justifyContent: "center"
   },
-  question: {fontSize: 20, textAlign: 'center'},
+  question: { fontSize: 20, textAlign: "center" }
 });
 
 export default class StressTest extends Component {
   static navigationOptions = {
-    header: null,
+    header: null
   };
 
   // Set State
@@ -66,7 +66,7 @@ export default class StressTest extends Component {
       stressCount: 0,
       questionAnswered: 0,
       // buttonClicked: false,
-      buttonCol: ['#6bccf3', '#936df4', '#6d8bf4', '#6de5f4'],
+      buttonCol: ["#6bccf3", "#936df4", "#6d8bf4", "#6de5f4"]
       // showResultsModal: false,
     };
     this.handleResponse = this.handleResponse.bind(this);
@@ -88,68 +88,68 @@ export default class StressTest extends Component {
     console.info("I'm being clicked");
     this.setState(
       {
-        questionAnswered: this.state.questionAnswered + 1,
+        questionAnswered: this.state.questionAnswered + 1
       },
       () => {
         if (this.state.questionIndex < dassQuestions.length) {
-          console.info('Click Number:' + this.state.questionAnswered);
+          console.info("Click Number:" + this.state.questionAnswered);
           // Increment index of array
           this.setState({
-            questionIndex: this.state.questionIndex + 1,
+            questionIndex: this.state.questionIndex + 1
           });
           // Determine DAS and add to each counter
           switch (dassQuestions[this.state.questionIndex].das) {
-            case 'd':
+            case "d":
               this.setState(
                 {
-                  depressionCount: this.state.depressionCount + dassValue,
+                  depressionCount: this.state.depressionCount + dassValue
                 },
                 () => {
                   console.info(
-                    'depressionCount: ' + this.state.depressionCount,
+                    "depressionCount: " + this.state.depressionCount
                   );
                   if (this.state.questionAnswered === dassQuestions.length) {
                     this.getResults();
                     this.resetStates();
                   }
-                },
+                }
               );
               break;
-            case 'a':
+            case "a":
               this.setState(
                 {
-                  anxietyCount: this.state.anxietyCount + dassValue,
+                  anxietyCount: this.state.anxietyCount + dassValue
                 },
                 () => {
-                  console.info('anxietyCount: ' + this.state.anxietyCount);
+                  console.info("anxietyCount: " + this.state.anxietyCount);
                   if (this.state.questionAnswered === dassQuestions.length) {
                     this.getResults();
                     this.resetStates();
                   }
-                },
+                }
               );
               break;
-            case 's':
+            case "s":
               this.setState(
                 {
-                  stressCount: this.state.stressCount + dassValue,
+                  stressCount: this.state.stressCount + dassValue
                 },
                 () => {
-                  console.info('stressCount: ' + this.state.stressCount);
+                  console.info("stressCount: " + this.state.stressCount);
                   if (this.state.questionAnswered === dassQuestions.length) {
                     this.getResults();
                     this.resetStates();
                   }
-                },
+                }
               );
               break;
           }
           // Change button colors
           // this.changeBtnCol();
         } else {
-          console.info('ELSE');
+          console.info("ELSE");
         }
-      },
+      }
     );
   }
 
@@ -161,9 +161,9 @@ export default class StressTest extends Component {
       anxietyCount: 0,
       stressCount: 0,
       questionAnswered: 0,
-      buttonCol: ['#6bccf3', '#936df4', '#6d8bf4', '#6de5f4'],
-    })
-  }
+      buttonCol: ["#6bccf3", "#936df4", "#6d8bf4", "#6de5f4"]
+    });
+  };
 
   // Calculate results
   getResults = () => {
@@ -175,10 +175,10 @@ export default class StressTest extends Component {
     //     stressCount: this.state.stressCount * 2,
     //   },
     // () => {
-    this.props.navigation.navigate('DassResults', {
+    this.props.navigation.navigate("DassResults", {
       depressionCount: this.state.depressionCount * 2,
       anxietyCount: this.state.anxietyCount * 2,
-      stressCount: this.state.stressCount * 2,
+      stressCount: this.state.stressCount * 2
     });
   };
 
@@ -188,22 +188,24 @@ export default class StressTest extends Component {
 
     return (
       <View style={styles.background}>
-        <View style={{flex: 1, justifyContent: 'center'}}>
-          <View style={{flex: 1, justifyContent: 'center'}}>
+        <View style={{ flex: 1, justifyContent: "center" }}>
+          <View style={{ flex: 1, justifyContent: "center" }}>
             <ImageBackground
               style={styles.logo}
-              source={require('../../assets/images/stresstest/upsyDABss.png')}
-              resizeMode={'contain'}
-              width="95%">
+              source={require("../../assets/images/stresstest/upsyDABss.png")}
+              resizeMode={"contain"}
+              width="95%"
+            >
               <View style={styles.questionContainer}>
                 <Text
                   style={{
-                    fontWeight: 'bold',
+                    fontWeight: "bold",
                     paddingBottom: 5,
                     marginLeft: 50,
                     fontSize: 17,
-                    width: 300,
-                  }}>
+                    width: 300
+                  }}
+                >
                   How relevant is this statement?
                 </Text>
                 <Text style={styles.question}>
@@ -216,7 +218,9 @@ export default class StressTest extends Component {
           </View>
         </View>
         <View />
-        <View style={{flex: 1, justifyContent: 'center', marginBottom: '10%'}}>
+        <View
+          style={{ flex: 1, justifyContent: "center", marginBottom: "10%" }}
+        >
           <View>
             <DassResponse
               buttonCol={this.state.buttonCol[0]}
