3f8806c2748111778b57c5aaa6e74b9c02ef8882
Emily Baumgartner <emily.r.baum@gmail.com>
2019-10-07 23:00:04 -0400

    background color for typing text

--- components/TypingIndicator/index.js
+++ components/TypingIndicator/index.js
@@ -3,10 +3,12 @@ import { StyleSheet, View, Text } from "react-native";
 
 const styles = StyleSheet.create({
     footerContainer: {
-        padding: 5
+        padding: 5,
+        paddingLeft: 15,
+        backgroundColor: '#f2f2f2'
     },
     footerText: {
-        color: 'blue'
+        color: 'black'
     }
   });
 
